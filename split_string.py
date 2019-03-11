cab98f1dffef0ef9f2137d803db2413e634c0a98
Marco <marcodifran@gmail.com>
2019-03-11 16:29:59 +0100

    Table counter added

--- split_string.py
+++ split_string.py
@@ -1,8 +1,7 @@
 import re
 
 cellDatePosition = "<Cell R1C7 '2019-03-08'>"
-print(cellDatePosition)
-cellDatePosition.split()
-print(re.findall(r'R(\d+)',cellDatePosition))
-print(re.findall(r'C(\d+)',cellDatePosition))
+# take first number after R and C, (it is in an array beacuse of re)
+row = int(re.findall(r'R(\d+)',cellDatePosition)[0])
+col = int(re.findall(r'C(\d+)',cellDatePosition)[0])
 
--- spreadsheets.py
+++ spreadsheets.py
@@ -4,17 +4,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
 
+# login to spreadsheet
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-
 gc = gspread.authorize(ServiceAccountCredentials.from_json_keyfile_name('credentials.json',scope))
 sheet = gc.open("Voti statistica").sheet1
+
+# get email and matricola values from the page
 email = sheet.cell(2,1).value
 matricola = sheet.cell(2,2).value
+print(email + ' ' + matricola)
 
-print(email)
-print(matricola)
-
+# access statistica page
 url = 'http://datascience.maths.unitn.it/ocpu/library/doexercises/www/'
 driver = webdriver.Chrome(r"D:\Users\Marco\Downloads\chromedriver.exe")
 driver.get(url)
@@ -36,6 +38,12 @@ driver.find_element_by_id('link_res').click()
 wait.until(EC.visibility_of_element_located((By.ID, "results_table")))
 testDate = driver.find_element_by_xpath('//*[@id="results_table"]/tbody/tr[1]/td[2]').text
 selectedMark = driver.find_element_by_xpath('//*[@id="results_table"]/tbody/tr[1]/td[3]').text
-print(testDate)
-print(selectedMark)
+print(testDate + ' - ' + selectedMark)
 
+# count rows and cols
+rowCount = re.findall("<tr>",driver.page_source)
+print(rowCount)
+# for size table, get date and mark (this page)
+# insert them into spreadsheet (sheets.py)
+
+driver.close()
