23eee7d0626175900369894e76f4140df8be780d
Marco <marcodifran@gmail.com>
2019-03-12 10:04:32 +0100

    FIrst working version release

--- split_string.py
+++
@@ -1,7 +0,0 @@
-import re
-
-cellDatePosition = "<Cell R1C7 '2019-03-08'>"
-# take first number after R and C, (it is in an array beacuse of re)
-row = int(re.findall(r'R(\d+)',cellDatePosition)[0])
-col = int(re.findall(r'C(\d+)',cellDatePosition)[0])
-
--- spreadsheets.py
+++ spreadsheets.py
@@ -11,41 +11,43 @@ scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/aut
 gc = gspread.authorize(ServiceAccountCredentials.from_json_keyfile_name('credentials.json',scope))
 sheet = gc.open("Voti statistica").sheet1
 
-# get email and matricola values from the page
-email = sheet.cell(2,1).value
-matricola = sheet.cell(2,2).value
-print(email + ' ' + matricola)
-
 # access statistica page
 url = 'http://datascience.maths.unitn.it/ocpu/library/doexercises/www/'
 driver = webdriver.Chrome(r"D:\Users\Marco\Downloads\chromedriver.exe")
 driver.get(url)
 
-# wait for the page to be loaded
-wait = WebDriverWait(driver, 15)
-wait.until(EC.visibility_of_element_located((By.ID, "email_address")))
+for student in range(2,8):
+  # wait for the page to be loaded
+  wait = WebDriverWait(driver, 15)
+  wait.until(EC.visibility_of_element_located((By.ID, "email_address")))
 
-# write values in the page
-driver.find_element_by_id('email_address').send_keys(email)
-driver.find_element_by_id('matricola').send_keys(matricola)
-driver.find_element_by_id('signin').click()
+  # get email and matricola values from the page
+  email = sheet.cell(student,1).value
+  matricola = sheet.cell(student,2).value
+  print('Login: ' + email + ' ' + matricola)
 
-# click for table with marks
-wait.until(EC.visibility_of_element_located((By.ID, "link_res")))
-driver.find_element_by_id('link_res').click()
+  # write values in the page
+  driver.find_element_by_id('email_address').send_keys(email)
+  driver.find_element_by_id('matricola').send_keys(matricola)
+  driver.find_element_by_id('signin').click()
 
-# count rows and cols
-wait.until(EC.visibility_of_element_located((By.ID, "results_table")))
-rowCount = len(re.findall("<tr>",driver.page_source))-1 # -1 to remove thead
+  # click for table with marks
+  wait.until(EC.visibility_of_element_located((By.ID, "link_res")))
+  driver.find_element_by_id('link_res').click()
 
-for x in range(rowCount):
-  print(x+1)
-  datePath = '//*[@id="results_table"]/tbody/tr['+str(x)+']/td[2]'
-  markPath = '//*[@id="results_table"]/tbody/tr['+str(x)+']/td[3]'
-  selectedDate = driver.find_element_by_xpath(datePath).text
-  selectedMark = driver.find_element_by_xpath(markPath).text
-  print(selectedDate + ' - ' + selectedMark)
-# for size table, get date and mark (this page)
-# insert them into spreadsheet (sheets.py)
+  # count rows and cols
+  wait.until(EC.visibility_of_element_located((By.ID, "results_table")))
+  rowCount = len(re.findall("<tr>",driver.page_source))-1 # -1 to remove thead
+
+  for x in range(1,rowCount+1):
+    datePath = '//*[@id="results_table"]/tbody/tr['+str(x)+']/td[2]'
+    markPath = '//*[@id="results_table"]/tbody/tr['+str(x)+']/td[3]'
+    selectedDate = driver.find_element_by_xpath(datePath).text
+    selectedMark = driver.find_element_by_xpath(markPath).text
+    print(selectedDate + ' - ' + selectedMark)
+    cellDatePosition = str(sheet.findall(selectedDate))
+    col = int(re.findall(r'C(\d+)',cellDatePosition)[0])
+    sheet.update_cell(student,col,selectedMark)
+  driver.refresh()
 
 driver.close()
