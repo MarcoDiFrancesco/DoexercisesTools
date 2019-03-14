4a41af4468dbfb83cf774da744d1e582025fa6fb
Marco <marcodifran@gmail.com>
2019-03-14 18:50:43 +0100

    Added check users from existance spreadsheet

--- checkUsers.py
+++ checkUsers.py
@@ -9,46 +9,45 @@ import re
 # login to spreadsheet
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 gc = gspread.authorize(ServiceAccountCredentials.from_json_keyfile_name('credentials.json',scope))
-sheet = gc.open("Voti statistica").get_worksheet(1)
+sheet = gc.open("Voti statistica").get_worksheet(2)
 
 # access statistica page
 url = 'http://datascience.maths.unitn.it/ocpu/library/doexercises/www/'
 driver = webdriver.Chrome(r"D:\Users\Marco\Downloads\chromedriver.exe")
 driver.get(url)
 
-for student in range(2,500):
+wait = WebDriverWait(driver, 20)
+
+for student in range(1,500):
   try:
     # wait for the page to be loaded
-    wait = WebDriverWait(driver, 20)
     wait.until(EC.visibility_of_element_located((By.ID, "email_address")))
 
     # get email and matricola values from the page
-    email = sheet.cell(student,1).value
-    matricola = sheet.cell(student,2).value
-    print('Login: ' + email + ' ' + matricola)
+    matricola = sheet.cell(student,1).value
+    cognome = sheet.cell(student,2).value
+    nome = sheet.cell(student,3).value
+
+    print('Login: ' + nome + ' ' + cognome + ' ' + matricola)
+
+    email = nome.lower()+'.'+cognome.lower()
+    email = email.replace(" ","")
 
     # write values in the page
     driver.find_element_by_id('email_address').send_keys(email)
     driver.find_element_by_id('matricola').send_keys(matricola)
     driver.find_element_by_id('signin').click()
 
-    # click for table with marks
-    wait.until(EC.visibility_of_element_located((By.ID, "link_res")))
-    driver.find_element_by_id('link_res').click()
+    try:
+      wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="formindex"]/div[1]/div')))
+      sheet.cell(student,4).value='not found'
+      print('not found')
+    except:
+      sheet.cell(student,4).value = "found"
+      print('found')
 
-    # count rows and cols
-    wait.until(EC.visibility_of_element_located((By.ID, "results_table")))
-    rowCount = len(re.findall("<tr>",driver.page_source))-1 # -1 to remove thead
+# NOT WORKING WHEN I TRY TO WRITE A VALUE IN SPREADSHEET
 
-    for x in range(1,rowCount+1):
-      datePath = '//*[@id="results_table"]/tbody/tr['+str(x)+']/td[2]'
-      markPath = '//*[@id="results_table"]/tbody/tr['+str(x)+']/td[3]'
-      selectedDate = driver.find_element_by_xpath(datePath).text
-      selectedMark = driver.find_element_by_xpath(markPath).text
-      print(selectedDate + ' - ' + selectedMark)
-      cellDatePosition = str(sheet.findall(selectedDate))
-      col = int(re.findall(r'C(\d+)',cellDatePosition)[0])
-      sheet.update_cell(student,col,selectedMark)
     driver.refresh()
   except:
     print('Done')
